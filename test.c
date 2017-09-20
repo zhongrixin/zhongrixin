594bea8bfc7042aff66ec7fa4c77cf9b1f6c1206
zrx <2449136436@qq.com>
2017-09-20 11:51:23 +0800

    add test.c

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int a, b;
+    while(a < 10)
+    {
+
+       printf("a = %d  b = %d;\n", ++a, b++);
+
+    }
+    
+    return 0;
+}
